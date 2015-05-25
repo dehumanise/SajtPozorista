b91e2b0ec726868d35ec06d0dc6f8ec0aac9ea28
dehumanise <dehumanise@gmail.com>
2015-05-25 18:27:38 +0200

    Update Predstave.php

--- Faza5/SajtPozorista/application/controllers/Predstave.php
+++ Faza5/SajtPozorista/application/controllers/Predstave.php
@@ -2,45 +2,58 @@
 
 include_once APPPATH . '/controllers/Base.php';
 
-class Vesti extends Base {
+class Predstave extends Base {
 
     private $viewIndicator;
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('vesti_m');
+        $this->load->model('predstave_m');
+        /*$this->load->model('pozorista_m');*/
         $this->load->helper('form');
     }
 
     protected function obrada($data = NULL) {
         if ($this->viewIndicator === 'View') {
-            $this->load->view('templates/' . $this->userRole . '/vest', $data);
+            $this->load->view('templates/' . $this->userRole . '/predstava', $data);
+        } elseif ($this->viewIndicator === 'ViewSpecific') {
+            $this->load->view('templates/' . $this->userRole . '/predstave', $data);
         } elseif ($this->viewIndicator === 'Insert') {
-            $this->load->view('templates/dodajVest', $data);
+            /*$data['pozorista'] = $this->predstave_m->find();*/
+            $data['pozorista'] = array(0 => array("PozID" => "1","Naziv" => "Srpsko Narodno Pozoriste"),1 => array("PozID" => "2","Naziv" => "Neko Drugo Pozoriste"));
+            $this->load->view('templates/dodajPredstavu', $data);
         } elseif ($this->viewIndicator === 'Update') {
-            $this->load->view('templates/izmeniVest', $data);
+            /*$data['pozorista'] = $this->predstave_m->find();*/
+            $data['pozorista'] = array(0 => array("PozID" => "1","Naziv" => "Srpsko Narodno Pozoriste"),1 => array("PozID" => "2","Naziv" => "Neko Drugo Pozoriste"));
+            $this->load->view('templates/izmeniPredstavu', $data);
         } else {
-            $data['vesti'] = $this->vesti_m->find();
-            $this->load->view('templates/' . $this->userRole . '/vesti', $data);
+            $data['predstave'] = $this->predstave_m->find();
+            $this->load->view('templates/' . $this->userRole . '/predstave', $data);
         }
     }
 
-    public function vest($id) {
-        $this->viewIndicator = 'View';
-        $data['vest'] = $this->vesti_m->findOne($id);
+    public function predstave($id) {
+        $this->viewIndicator = 'ViewSpecific';
+        $data['predstave'] = $this->predstave_m->findSpecific($id);
         $this->view($data);
+    } 
+
+    public function predstava($id) {
+        $this->viewIndicator = 'View';
+        $data['predstava'] = $this->predstave_m->findOne($id);
+        $this->view($data); 
     }
 
-    public function dodaj($data = NULL) {
+    public function dodaj() {
         if (!checkPermission(array('moderator', 'admin'), $this->userRole)) {
             redirect(route_url(''));
         } else {
             $this->viewIndicator = 'Insert';
-            $this->view($data);
+            $this->view();
         }
     }
 
-    public function dodajVest() {
+    public function dodajPredstavu() {
         if (!checkPermission(array('moderator', 'admin'), $this->userRole)) {
             redirect(route_url(''));
         } else {
@@ -48,7 +61,7 @@ class Vesti extends Base {
             if ($this->form_validation->run() === FALSE) {
                 $this->dodaj();
             } else {
-                $this->loadUploadLibrary('vesti/');
+                $this->loadUploadLibrary('predstave/');
                 //Uploading image is NOT required!
                 //So, first I check if the file exists
                 //Only if the image IS uploaded AND there is an error in uploading I report error
@@ -58,9 +71,9 @@ class Vesti extends Base {
                     $this->dodaj($data);
                 } else {
                     $image = $this->upload->data();
-                    $ret = $this->vesti_m->insert($this->session->username, $image['file_name']);
+                    $ret = $this->predstave_m->insert($this->session->username, $image['file_name']);
                     if ($ret) {
-                        redirect(route_url('vesti/view'));
+                        redirect(route_url('predstave/view'));
                     } else {
                         $this->dodaj();
                     }
@@ -74,44 +87,43 @@ class Vesti extends Base {
             redirect(route_url(''));
         } else {
             $this->viewIndicator = 'Update';
-            $data['vest'] = $this->vesti_m->findOne($id);
+            $data['predstava'] = $this->predstave_m->findOne($id);
             $this->view($data);
         }
     }
 
-    public function izmeniVest() {
+    public function izmeniPredstavu() {
         if (!checkPermission(array('moderator', 'admin'), $this->userRole)) {
             redirect(route_url(''));
         } else {
             $this->validation();
             if ($this->form_validation->run() === FALSE) {
-                $this->izmeni($this->input->post('VestID'));
+                $this->izmeni($this->input->post('PredID'));
             } else {
-                $this->loadUploadLibrary('vesti/');
+                $this->loadUploadLibrary('predstave/');
                 if ($_FILES['slika'] && ($_FILES['slika']['size'] > 0) && !$this->upload->do_upload('slika')) {
                     $data = array('error' => $this->upload->display_errors());
                     $this->dodaj($data);
                 } else {
                     $image = $this->upload->data();
-                    $fileName = $image['file_name'] ? $image['file_name'] : NULL;
-                    $this->vesti_m->update($fileName);
-                    redirect(route_url('vesti/vest/' . $this->input->post('VestID')));
+                    $fileName = $image['file_name'] ? $image['file_name'] : $_FILES['slika']['name'];
+                    $this->predstave_m->update($fileName);
+                    redirect(route_url('predstave/predstava/' . $this->input->post('PredID')));
                 }
             }
         }
     }
 
     private function validation() {
-        $this->form_validation->set_rules('naslov', 'Naslov', 'required');
-        $this->form_validation->set_rules('sadrzaj', 'Sadrzaj', 'required');
+        $this->form_validation->set_rules('naziv', 'Naziv', 'required');
     }
 
     public function obrisi($id) {
         if (!checkPermission(array('moderator', 'admin'), $this->userRole)) {
             redirect(route_url(''));
         } else {
-            $this->vesti_m->removeOne($id);
-            redirect('vesti/view');
+            $this->predstave_m->removeOne($id);
+            redirect('predstave/view');
         }
     }
 
